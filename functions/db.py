27d99102780e95c34caaf3f2bca2c7f7303918c3
Zedaine McDonald <zedaine.mcdonald@mussongroup.com>
2025-11-28 20:24:38 -0500

    ..

--- functions/db.py
+++ functions/db.py
@@ -39,18 +39,6 @@ def get_db():
     target_path = home_lib/"private"
     dbsecrets = st.secrets["MYSQL"]
     
-    
-
-    #if target_path.is_dir():
-    #    """Returns a MySQL connection using Streamlit secrets"""
-        
-        #ssl authentication.
-    #    ssl_options = {
-    #    "ca": dbsecrets["sslserverca"],
-    #    "cert": dbsecrets["sslclientcert"],
-    #   "key": dbsecrets["sslclientkey"],
-    #   "check_hostname": dbsecrets["sslcheck_hostname"]
-    #    }
 
     try:
         connection = pymysql.connect(
@@ -66,35 +54,6 @@ def get_db():
     except pymysql.Error as e:
         st.error(f"Error connecting to MySQL database: {e}")
         return None
-#else:
-#    ca_path = write_cert(dbsecrets["sslserverca_b64"], "server-ca.pem")
-#    cert_path = write_cert(dbsecrets["sslclientcert"], "client-cert.pem")
-#    key_path = write_cert(dbsecrets["sslclientkey"], "client-key.pem")
-
-
-#    ssl_options = {
-#        "ca": ca_path,
-#        "cert": cert_path,
-#        "key": key_path,
-#        "check_hostname": False
-#    }
-#    try:
-#        connection = pymysql.connect(
-#            host=st.secrets["MYSQL"]["host"],
-#            user=st.secrets["MYSQL"]["user"],
-#            password=st.secrets["MYSQL"]["password"],
-#            database=st.secrets["MYSQL"]["database"],
-#            cursorclass=pymysql.cursors.DictCursor,
-#            autocommit=True,
-#            charset="utf8mb4",
-#            ssl = ssl_options
-#        )
-#        return connection
-    except pymysql.Error as e:
-        st.error(f"Error connecting to MySQL database: {e}")
-        return None
-
-
 
 
 # Users CRUD operations
